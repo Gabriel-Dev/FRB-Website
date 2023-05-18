7bcf5e1328bd989c256fe8a6e44deb22ca0fc655
Lucas <lucaspedrosadesouza@gmail.com>
2023-05-18 16:35:05 -0300

    feat/CustomerArea

--- FRB-Website/src/components/input/index.jsx
+++ FRB-Website/src/components/input/index.jsx
@@ -1,5 +1,6 @@
 import { Label } from "./styled";
 
+// eslint-disable-next-line react/prop-types
 export const Input = ({ type, name, label, placeholder, className }) => {
   return (
     <Label> <span>{label}</span>
