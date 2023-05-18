eddbcecabf484ae3a658e40c5d001142923ef295
Gabriel <gabr.lima7@gmail.com>
2023-05-18 13:46:56 -0300

    fix: header and footer

--- FRB-Website/src/components/header/styled.js
+++ FRB-Website/src/components/header/styled.js
@@ -7,16 +7,14 @@ height: 100px;
 display: flex;
 align-items: center;
 justify-content: center;
-position: sticky;
+position: fixed;
 top: 0;
 left: 0;
 z-index: 2;
 
-background-color: var(--color-grey-1);
-box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.3);
 
 .container{
-    width: 100%;
+    width: 1440px;
     height: 100%;
 
     display: flex;
